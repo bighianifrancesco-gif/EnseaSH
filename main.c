f390dc82da31d19261f21fef450f25aa890f1f57
FranMhmmd <bighiani.francesco@gmail.com>
2025-12-11 11:56:39 +0100

    Q2; it works

--- main.c
+++ main.c
@@ -1,18 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
+#define buffer_size 128
 #define WelcomeMSG "$ ./enseaSH \nWelcome to ENSEA Tiny Shell. \nType 'exit' to quit. \n"
 #define Prompt "enseash %"
+
 static void print_welcome(void) {
     write(STDOUT_FILENO, WelcomeMSG, strlen(WelcomeMSG));
-    }
+}
+
 static void print_prompt(void) {
     write(STDOUT_FILENO, Prompt, strlen(Prompt));
 }
 
+int main(void) {
+    int status;
+    char buffer[buffer_size];
 
-int main() {
     print_welcome();
     print_prompt();
+
+    while ((read(STDIN_FILENO, buffer, buffer_size)) > 0) {
+        buffer[strcspn(buffer, "\n")] = '\0';  // Remove newline character
+
+        if (strlen(buffer) == 0) {
+            print_prompt();
+            continue;
+        }
+
+        if (strcmp(buffer, "exit") == 0) {
+            break;
+        }
+
+        pid_t pid = fork();
+        if (pid != 0) {
+            waitpid(pid, &status, 0);
+            print_prompt();
+        } else {
+            char *argv[] = {buffer, NULL};
+            execvp(buffer, argv);
+            perror("execvp failed");
+            exit(1);
+        }
+    }
+
+    return 0;
 }
